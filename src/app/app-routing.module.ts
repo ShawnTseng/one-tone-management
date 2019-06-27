3a8f08e356d289892421b31833c4916f63af4b2b
ShawnTseng <shawntseng40@gmail.com>
2019-06-27 10:48:59 +0800

    lazy loading 測試

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { StudentComponent } from './student/student.component';
 import { TeacherComponent } from './teacher/teacher.component';
 import { CourseComponent } from './course/course.component';
 
 const routes: Routes = [
-  { path: 'student', component: StudentComponent },
+  { path: 'student', loadChildren: () => import('./student/student.module').then(mod => mod.StudentModule) },
   { path: 'teacher', component: TeacherComponent },
   { path: 'course', component: CourseComponent },
   { path: '**', redirectTo: '' }
