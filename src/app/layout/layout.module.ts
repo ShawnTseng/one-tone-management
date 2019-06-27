0f1757b1f38108afd1fcc779163401c172db86c3
ShawnTseng <shawntseng40@gmail.com>
2019-06-27 10:29:09 +0800

    新增介面&路由: 1.老師 2.學生

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { StudentModule } from '../student/student.module';
+import { TeacherModule } from '../teacher/teacher.module';
 
 @NgModule({
   declarations: [TopBarComponent],
   imports: [
-    CommonModule, MatIconModule, MatButtonModule, MatToolbarModule
+    CommonModule, RouterModule,
+    MatIconModule, MatButtonModule, MatToolbarModule,
+    StudentModule, TeacherModule
   ],
   exports: [
     TopBarComponent
