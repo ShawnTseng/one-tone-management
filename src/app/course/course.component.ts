472b6ca3d0ab25688289b98267b2533446b3c65a
ShawnTseng <shawntseng40@gmail.com>
2019-07-05 10:15:18 +0800

    新增: 行事曆功能測試

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EventInput } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGrigPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-course',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.scss']
 })
 export class CourseComponent implements OnInit {
+  calendarPlugins = [dayGridPlugin, timeGrigPlugin, interactionPlugin]; // important!
+
+  calendarEvents: EventInput[] = [
+    { title: 'event 1', date: Date.now() }
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  handleDateClick(arg) {
+    if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
+      this.calendarEvents = this.calendarEvents.concat({ // add new event data. must create new array
+        title: 'New Event',
+        start: arg.date,
+        allDay: arg.allDay
+      });
+    }
+  }
 }
--- src/app/course/course.module.ts
+++ src/app/course/course.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FullCalendarModule } from '@fullcalendar/angular';
 import { CourseComponent } from './course.component';
 import { CourseRoutingModule } from './course-routing.module';
 
 @NgModule({
   declarations: [CourseComponent],
   imports: [
-    CommonModule, CourseRoutingModule
+    CommonModule, CourseRoutingModule, FullCalendarModule
   ]
 })
 export class CourseModule { }
